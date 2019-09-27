87415e6724f3374438be65157cbf5112efc5e8f5
OlawaleJoseph <adedekojoseph@gmail.com>
2019-09-27 01:17:35 +0100

    travis fix

--- src/middlewares/auth/auth.js
+++ src/middlewares/auth/auth.js
@@ -6,7 +6,7 @@ import { jwtVerifyUserToken } from '../../utils/index';
 export const authorization = async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
-    return res.status(401).json({
+    return res.status(407).json({
       success: false,
       message: 'Token required',
     });
@@ -21,7 +21,7 @@ export const authorization = async (req, res, next) => {
     }
     throw new Error('Invalid Token Provided');
   } catch (error) {
-    return res.status(407).json({
+    return res.status(401).json({
       success: false,
       message: error.message,
     });
