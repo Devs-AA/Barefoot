d6496ab7a5a51cd29aa4ad931394e1f8f56e15a0
OlawaleJoseph <adedekojoseph@gmail.com>
2019-10-14 03:47:16 +0100

    create notification model and migration

---
+++ src/models/notifitications.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const notifitications = sequelize.define('notifitications', {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING,
+    recipientId: DataTypes.INTEGER
+  }, {});
+  notifitications.associate = (models) => {
+    notifitications.belongsTo(models.users, {
+      primaryKey: 'recipientId',
+      as: 'notifications'
+    });
+  };
+  return notifitications;
+};
